9fb989e5b96aa92d42c91d484ff6067d666cb15c
Kat WynneEllis <k.wynneellis@gmail.com>
2023-02-22 12:08:20 +0000

    generated 5 seed restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+puts "Creating restaurants..."
+
+5.times do
+  restauarnt = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: categories.sample
+  )
+  puts "generated #{restauarnt.name}"
+end
+
+puts 'Finished!'
